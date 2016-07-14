bb71de9829582f2963d8db18a04697ccd2100ac1
Amanda Olczak <aolczak@gmail.com>
2016-07-14 16:46:17 -0400

    push

--- js/script1.js
+++ js/script1.js
@@ -1,6 +1,10 @@
 //javascript / jQuery for our project!
 $(document).ready(function() {});
+
+var $seatSelected;
+var $submit = $("#submit");
 var $seat = $(".seat");
+var users = [];
 var $regForm = $("#reg");
 var $reservedSeat = $(".reserved");
 
@@ -15,11 +19,38 @@ $($regForm).hide();
 $($seat).on("click", function() {
     if ($($seat.not(".reserved"))) {
         $($regForm).fadeIn(2500);
+        $seatSelected = $(this);
     }
 });
 
 
 
-$($reservedSeat).hover(function() {
-    $customerInfo.fadeIn(1500);
+
+
+  function User(name, email, seatNumber) {
+    this.name = name;
+    this.email = email;
+    this.seatNumber = seatNumber;
+  };
+
+  function createUser(){
+  var name = $("#name").val();
+  console.log(name);
+  var email = $("#email").val();
+  var seatNumber = $seatSelected.attr("id");
+  var user = new User(name, email, seatNumber);
+  users.push(user);
+  }
+
+  $regForm.on("submit", function(event){
+    event.preventDefault();
+    var seatNumber = $seatSelected.attr("id");
+    console.log(seatNumber);
+    createUser();
+    $seatSelected.addClass("reserved");//add class to the element not the id value
+  });
+
+  $($reservedSeat).hover(function() {
+      $customerInfo.fadeIn(1500);
+
 });
--- js/script3.js
+++ js/script3.js
@@ -1,26 +1,28 @@
-
-$(document).ready(function(){
-
-  var $seatSelected;
-  var $submit = $("#submit");
-  var $seat = $(".seat");
-
-  function User(name, email, seatNumber) {
-      this.name = name;
-      this.email = email;
-      this.seatNumber = seatNumber;
-  };
-
-  function createUser(){
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var user = new User(name, email, seatNumber);
-  }
-
-    $submit.on("submit", function(){
-      var seatNumber = $seatSelected.attr("id");
-      createUser();
-      seatNumber.addClass("reserved");
-    });
-
-});
+// 
+// $(document).ready(function(){
+//
+//   var $seatSelected;
+//   var $submit = $("#submit");
+//   var $seat = $(".seat");
+//   var users = [];
+//
+//   function User(name, email, seatNumber) {
+//       this.name = name;
+//       this.email = email;
+//       this.seatNumber = seatNumber;
+//   };
+//
+//   function createUser(){
+//     var name = $("#name").val();
+//     var email = $("#email").val();
+//     var user = new User(name, email, seatNumber);
+//     users.push(user);
+//   }
+//
+//     $submit.on("submit", function(){
+//       var seatNumber = $seatSelected.attr("id");
+//       createUser();
+//       seatNumber.addClass("reserved");
+//     });
+//
+// });
