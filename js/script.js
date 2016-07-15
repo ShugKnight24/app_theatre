feea0a0707a555f9fcea64996aa168122009e50c
Shugmi Shumunov <SShumunov@gmail.com>
2016-07-15 09:05:27 -0400

    Remove seat IDs. Remove asterik.

--- js/script.js
+++ js/script.js
@@ -81,8 +81,8 @@ $(document).ready(function() {
 });
 
 
-* //change text content of seats from "seat" to seat number
-* //use seat text content to get seat number instead of id && remove id from seats
+
+//use seat text content to get seat number instead of id && remove id from seats
 //on click on available seat, change background color
 //toggle selected seats
 //add multiple seats
