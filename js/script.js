9f1df1ecad51c1ff27d2f9ccb21e0b86d0e95327
Amanda Olczak <aolczak@gmail.com>
2016-07-15 09:21:03 -0400

    no changes

--- js/script.js
+++ js/script.js
@@ -1,88 +1,88 @@
 //javascript / jQuery for our project!
 $(document).ready(function() {
 
-  var $seatSelected;
-  var $submit = $("#submit");
-  var $seat = $(".seat");
-  var users = [];
-  var $regForm = $("#reg");
-  var $reservedSeat = $(".reserved");
-  // var $customerInfo = $(______ ? ? ? ? ? ______);
+    var $seatSelected;
+    var $submit = $("#submit");
+    var $seat = $(".seat");
+    var users = [];
+    var $regForm = $("#reg");
+    var $reservedSeat = $(".reserved");
+    // var $customerInfo = $(______ ? ? ? ? ? ______);
 
 
-  //hide form when page loads
+    //hide form when page loads
 
-  $($regForm).hide();
-
-
-/*-----------ON SEAT CLICK EVENT-------------*/
-
-  //Seat clicked event listener: if seat is not reserved, fadein form && store seat selected
-
-  $($seat).on("click", function() {
-      if ($($seat.not(".reserved"))) {
-          $($regForm).fadeIn(2500);
-          $seatSelected = $(this);          //Stores seat selected for use in submit event listener
-      }
-  });
+    $($regForm).hide();
 
 
-/*----------USER OBJECT CONSTRUCTOR---------*/
+    /*-----------ON SEAT CLICK EVENT-------------*/
 
+    //Seat clicked event listener: if seat is not reserved, fadein form && store seat selected
 
-  function User(name, email, seatNumber) {
-    this.name = name;
-    this.email = email;
-    this.seatNumber = seatNumber;
-  };
-
-
-/*----------ON FORM SUBMIT EVENT---------*/
-
-
-  // Creates object from user input and pushes object to array
-
-  function createUser(){
-  var name = $("#name").val();
-  console.log(name);
-  var email = $("#email").val();
-  var seatNumber = $seatSelected.children("p").text();
-  var user = new User(name, email, seatNumber);
-  console.log(user);
-  users.push(user);
-  console.log(users);
-  }
-
-  //Submit event listener: creates user object and adds "reserved" class to seat
-
-  $regForm.on("submit", function(event){
-    event.preventDefault();
-    createUser();
-    $seatSelected.addClass("reserved");
-    resetMouseover();
-  });
-
-
-  /*----------ON SEAT HOVER EVENT---------*/
-
-  function resetMouseover(){
-     $(".reserved").on("mouseover", function() {
-       var hoveredSeat = $(this).children("p").text();
-       var seatOwner;
-       console.log("hoveredSeat: " + hoveredSeat);
-       users.forEach(function(user){
-         if (user.seatNumber === hoveredSeat) {
-           seatOwner = user;
-         }
-       });
-       $(this).children("p").text(seatOwner.name);
+    $($seat).on("click", function() {
+        if ($($seat.not(".reserved"))) {
+            $($regForm).fadeIn(2500);
+            $seatSelected = $(this); //Stores seat selected for use in submit event listener
+        }
     });
-  }
+
+
+    /*----------USER OBJECT CONSTRUCTOR---------*/
+
+
+    function User(name, email, seatNumber) {
+        this.name = name;
+        this.email = email;
+        this.seatNumber = seatNumber;
+    };
+
+
+    /*----------ON FORM SUBMIT EVENT---------*/
+
+
+    // Creates object from user input and pushes object to array
+
+    function createUser() {
+        var name = $("#name").val();
+        console.log(name);
+        var email = $("#email").val();
+        var seatNumber = $seatSelected.children("p").text();
+        var user = new User(name, email, seatNumber);
+        console.log(user);
+        users.push(user);
+        console.log(users);
+    }
+
+    //Submit event listener: creates user object and adds "reserved" class to seat
+
+    $regForm.on("submit", function(event) {
+        event.preventDefault();
+        createUser();
+        $seatSelected.addClass("reserved");
+        resetMouseover();
+    });
+
+
+    /*----------ON SEAT HOVER EVENT---------*/
+
+    function resetMouseover() {
+        $(".reserved").on("mouseover", function() {
+            var hoveredSeat = $(this).children("p").text();
+            var seatOwner;
+            console.log("hoveredSeat: " + hoveredSeat);
+            users.forEach(function(user) {
+                if (user.seatNumber === hoveredSeat) {
+                    seatOwner = user;
+                }
+            });
+            $(this).children("p").text(seatOwner.name);
+        });
+    }
 });
 
 
-* //change text content of seats from "seat" to seat number
-* //use seat text content to get seat number instead of id && remove id from seats
+//change text content of seats from "seat" to seat number
+//use seat text content to get seat number instead of id && remove id from seats
 //on click on available seat, change background color
 //toggle selected seats
 //add multiple seats
