de1935348ca28c6a36746202b1cce297a0a5a671
Syed Rashid <rashidindigo@gmail.com>
2016-07-17 14:52:04 -0400

    Refactor script. Remove or replace redundant code. Make more readable, resuable, and maintainable. Organize script. Add comments.

--- js/script.js
+++ js/script.js
@@ -1,173 +1,164 @@
 $(function() {
 
-  var users = [];
-  var seatsSelected = [];
+  var users = [];           //Stores user information
+  var seatsSelected = [];   //Stores currently selected seats
+
+  //List of names used to randomly populate users array
   var randomUserNames = ["Charlie", "Benson", "Karen", "Katie", "Steve", "Erika", "Dave", "Ray", "Chelsea", "Ithica", "Jennifer", "Shanita", "Josh", "John", "Travis"];
 
+  /*-----------CACHE-------------*/
+
   var $seat = $(".seat");
-  var $regForm = $("#reg");
-  var $submit = $("#submit");
+  var $registrationForm = $("#registration");
+  var $seatingContainer = $("#seating-container");
 
+  /*-----------GENERAL TASKS-------------*/
 
-  //On page load
-  hideForm();
-  assignRandomUsers();
-  updateSeatOwner();
-
-
-  /*-----------RANDOMLY RESERVE SEATING ON PAGE LOAD-------------*/
-
-  function generateRandomInt(n) {
-    return Math.floor(Math.random() * (n + 1));
-  }
-
-  function addUser(name, email, seatNumber) {
-    var user = new User(name, email, seatNumber);
-    users.push(user);
-  }
-
-  function updateSeatOwner() {
-    users.forEach(function(user) {
-      $("#" + user.seatNumber).children(".owner-name").text(user.name);
-    });
-  }
-  
-  function assignRandomUsers() {
-    var isRandomlyAssigned, randomName, randomEmail, randomSeatNumber;
-    $seat.each(function() {
-      var isRandomlyAssigned = generateRandomInt(1);
-      if (isRandomlyAssigned) {
-        $(this).addClass("reserved");
-        var randomName = randomUserNames[Math.floor(Math.floor(Math.random() * 15))];
-        var randomEmail = randomName.toLowerCase() + "@example.com";
-        var randomSeatNumber = this.id;
-        var randomUser = new User(randomName, randomEmail, randomSeatNumber);
-        users.push(randomUser); //g
-      }
-    });
-  }
-
-
-
-  /*-----------ON SEAT CLICK EVENT-------------*/
-
-  //Seat clicked event listener: if seat is not reserved, fadein form && store seat selected
-
-  function showForm() {
-    $regForm.fadeIn(2500);
-  }
-
-  function manageSeatsSelected($currentSeat) {
-    var seatNumber = this.id;
-    var seatIndex = seatsSelected.findIndex(function(seat) {
-      return seat.attr("id") === seatNumber;
-    });
-    var notSelected = seatIndex === -1;
-
-    if (notSelected) {
-      seatsSelected.push($currentSeat);
-    } else {
-      seatsSelected.splice(seatIndex, 1);
-    }
-
-    $currentSeat.toggleClass("selected");
-  }
-
-  function scrollToForm() {
-    if (seatsSelected.length === 1) {
-        $('html,body').animate({ //Scrolls down to top of form when seat is selected
-            scrollTop: $regForm.offset().top
-        }, 'slow');
-    }
-  }
-
-  function hideForm() {
-    $regForm.hide();
-  }
-
-  function warnUser() {
-    alert("No sitting in other people's laps! Choose another seat.");
-  }
-
-  function clearForm() {
-    $regForm[0].reset();
-  }
-
-  $seat.on("click", function() {
-    var $currentSeat = $(this);
-    var notReserved = !($currentSeat.hasClass("reserved"));
-
-    if (notReserved) {
-      showForm();
-      manageSeatsSelected.call(this, $currentSeat);
-      scrollToForm();
-    } else {
-      hideForm();
-      warnUser();
-    }
-  });
-
-
-  /*----------USER OBJECT CONSTRUCTOR---------*/
-
-
+  //User object constructor
   function User(name, email, seatNumber) {
       this.name = name;
       this.email = email;
       this.seatNumber = seatNumber;
   };
 
+  //Adds user object to users array
+  function addUser(name, email, seatNumber) {
+    var user = new User(name, email, seatNumber);
+    users.push(user); //g
+  }
 
-  /*----------ON FORM SUBMIT EVENT---------*/
+  //Loops through users array and adds user names to seats
+  function updateSeatOwner() {
+    users.forEach(function(user) { //g
+      $("#" + user.seatNumber).children(".owner-name").text(user.name);
+    });
+  }
 
+  /*-----------RANDOM RESERVATION TASKS-------------*/
 
-  // Creates object from user input and pushes object to array
+  //Generates random integer between 0 and n (inclusive)
+  function generateRandomInt(n) {
+    return Math.floor(Math.random() * (n + 1));
+  }
 
+  //Randomly reserves seats, generates random users, and assigns users to seats
+  function assignRandomUsers() {
+    var isRandomlyAssigned, randomName, randomEmail, randomSeatNumber;
+
+    $seat.each(function() {
+      var isRandomlyAssigned = generateRandomInt(1);
+      if (isRandomlyAssigned) {
+        $(this).addClass("reserved");
+        randomName = randomUserNames[generateRandomInt(14)];
+        randomEmail = randomName.toLowerCase() + "@example.com";
+        randomSeatNumber = this.id;
+        addUser(randomName, randomEmail, randomSeatNumber);
+      }
+    });
+  }
+
+  /*-----------SEAT SELECTION TASKS-------------*/
+
+  //Adds/removes selection from queue && toggles .selected class
+  function manageSeatsSelected($currentSeat) {
+    var seatNumber = this.id;
+    var seatIndex = seatsSelected.findIndex(function(seat) { //g
+      return seat.attr("id") === seatNumber;
+    });
+    var notSelected = seatIndex === -1;
+
+    if (notSelected) {
+      seatsSelected.push($currentSeat); //g
+    } else {
+      seatsSelected.splice(seatIndex, 1); //g
+    }
+
+    $currentSeat.toggleClass("selected");
+  }
+
+  //Scrolls down to form on selection of seat (only after first selection)
+  function scrollToForm() {
+    if (seatsSelected.length === 1) { //g
+        $('html,body').animate({
+            scrollTop: $registrationForm.offset().top
+        }, 'slow');
+    }
+  }
+
+  //Notifies user that clicked seat is unavialable
+  function warnUser() {
+    alert("No sitting in other people's laps! Choose another seat.");
+  }
+
+  //Clears form inputs
+  function clearForm() {
+    $registrationForm[0].reset();
+  }
+
+  /*-----------SUBMIT SELECTION TASKS-------------*/
+
+  //Creates new user from registration form input values
   function createUser() {
       var name = $("#name").val();
       var email = $("#email").val();
       var seatNumber;
-      for (var i = 0; i < seatsSelected.length; i++) {  //use $ select+each instead of arr+for
+      for (var i = 0; i < seatsSelected.length; i++) { //g
           seatNumber = seatsSelected[i].attr("id");
-          var user = new User(name, email, seatNumber);
-          users.push(user);
+          addUser(name, email, seatNumber);
       }
   }
 
-  //Submit event listener: creates user object and adds "reserved" class to seat
+  /*-----------EVENT LISTENERS-------------*/
 
-  $regForm.on("submit", function(e) {
-      e.preventDefault();
-      createUser();
-      for (var i = 0; i < seatsSelected.length; i++) { //use $ select+each instead of arr+for
-          seatsSelected[i].addClass("reserved").removeClass("selected");
-      }
-      seatsSelected = [];                              //line redundant if using $ selection
-      updateSeatOwner();
-      clearForm();
-      hideForm();
+  //Handles seat selection
+  $seat.on("click", function() {
+    var $currentSeat = $(this);
+    var notReserved = !($currentSeat.hasClass("reserved"));
+
+    if (notReserved) {
+      $registrationForm.fadeIn(2500);
+      manageSeatsSelected.call(this, $currentSeat);
+      scrollToForm();
+    } else {
+      $registrationForm.hide();
+      warnUser();
+    }
   });
 
-  //Displays associated user info on mouseenter event on reserved seats
+  //Handles reservation form submission
+  $registrationForm.on("submit", function(e) {
+      e.preventDefault();
+      createUser();
+      for (var i = 0; i < seatsSelected.length; i++) { //g
+          seatsSelected[i].addClass("reserved").removeClass("selected");
+      }
+      seatsSelected = []; //g
+      updateSeatOwner();
+      clearForm();
+      $registrationForm.hide();
+  });
 
-  $("#seating-container").on("mouseenter mouseleave", ".reserved", function() {
+  //Handles hover events over seats
+  $seatingContainer.on("mouseenter mouseleave", ".reserved", function() {
     $(this).children(".owner-name").toggle();
   });
 
+  /*-----------INITIAL SETUP-------------*/
 
-
+  $registrationForm.hide();
+  assignRandomUsers();
+  updateSeatOwner();
 
 });
 
 
-//*priority* fix bug: each time form is submitted seat owner name is prepended to seat on hover an additional time
+
 
 //display selected seats on form
 //add available seats counter
-//making hover more uniform with seats vs seats+user info
+//make hover more uniform with seats vs seats+user info
 //add random colors for different users?
-//when seats are selected, then reserved seat is clicked on, the form is hidden until another         //available seat is clicked on
+//fix bug: when seats are selected, then reserved seat is clicked on, the form is hidden until another available seat is clicked on
 //add reserve confirmation
 //add name validation
-//fix bug: scrolls to form after every selection (should only scroll once)
 //is seatsSelected array necessary? Can a JQuery selection be used for .selected?
